9073066c23d16cbc7a63cf6dd6cfda8a848d9efa
tmax818 <tylermaxwell661@gmail.com>
2022-06-14 14:39:58 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # java_cd
+
+
+# assignments
+
+- [ ] [first java program](assignments/practice/first_java_program/README.md) 
---
+++ assignments/practice/first_java_program/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My home town is Burbank, CA");
+        
+    }
+}
